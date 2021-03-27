cb1bb8ecf183948304b1122770afdf18b78acb3f
Osama Saleh <43769315+Osama-Saleh@users.noreply.github.com>
2021-03-27 12:24:54 -0700

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,20 +1,15 @@
 # Import the Gtts module for text
 # to speech conversion
 from gtts import gTTS
-
-# import Os module to start the audio file
-import os
+from playsound import  playsound
 
 mytext = 'Wellcome to convert text to speech using python'
 
 # Language we want to use
-language = 'en'
+language='en'
 
+myobj=gTTS(text=mytext,lang=language,slow=True)
 
-myobj = gTTS(text=mytext, lang=language, slow=False)
-
-
-myobj.save("output.mp3")
-
-# Play the converted file
-os.system("start output.mp3")
+# Play the converted
+myobj.save("welcome1.mp3")
+playsound("welcome1.mp3")
